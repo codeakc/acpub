aec4ad042f0ac50922ef87a9d161e61ab257acb9
Abhishek Choubey <abhishekchoubey.oled@gmail.com>
2022-07-20 00:46:12 +0530

    add math directory and make it compilable in a library form

---
+++ cmake/math/math.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "math.hpp"
+using namespace std;
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ cmake/math/math.hpp
@@ -0,0 +1,4 @@
+#ifndef MATH
+#define MATH
+int add(int num1, int num2);
+#endif
