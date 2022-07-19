1e21d923a7485d82464357e0c3da35a1aaf4b6af
Abhishek Choubey <abhishekchoubey.oled@gmail.com>
2022-07-20 00:29:43 +0530

    Link hello world program with pthread lib

--- cmake/main.c
+++ cmake/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <pthread.h>
+static void *ptcb(void *arg)
+{
+	printf("%s: called\n", __func__);
+	return NULL;
+}
 int main(void)
 {
 	printf("Hello World!\n");
+	pthread_t pt;
+	if (pthread_create(&pt, NULL, ptcb, NULL))
+	{
+		printf("failed to create thread\n");
+		return 1;
+	}
+	int *ret;
+	if (pthread_join(pt, (void **)&ret))
+	{
+		printf("Failed to join thread\n");
+		return 1;
+	}
 	return 0;
 }
