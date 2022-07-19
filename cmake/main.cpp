cb2813b69620f8eb040fc6ef2c50de60a6543a0b
Abhishek Choubey <abhishekchoubey.oled@gmail.com>
2022-07-20 00:32:55 +0530

    change main.c to main.cpp

--- cmake/main.cpp
+++ cmake/main.cpp
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <iostream>
+
+using namespace std;
+
 static void *ptcb(void *arg)
 {
 	printf("%s: called\n", __func__);
@@ -8,6 +12,7 @@ static void *ptcb(void *arg)
 int main(void)
 {
 	printf("Hello World!\n");
+	cout<<"Hello World by cout"<<endl;
 	pthread_t pt;
 	if (pthread_create(&pt, NULL, ptcb, NULL))
 	{
